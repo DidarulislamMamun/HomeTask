5a099ffd5d7cebd62ad430e486e66448e37be2c0
Didarul <didarj6@gmail.com>
2024-11-23 01:07:56 +0600

    change Name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Card from "./Components/Card";
 
 function App() {
-
   // let myObj = {
   //   userName : "Didar",
   //   Narration : "lorem my name is didar",
@@ -11,13 +10,13 @@ function App() {
   //   userName : "Bappy",
   //   Narration : "lorem my name is Bappy",
   // }
-  
+
   return (
     <>
-      <h1 className="bg-green-400 text-blue-800 rounded-2xl mb-4">didar</h1>
+      <h1 className="bg-green-400 text-blue-800 rounded-2xl mb-4">Didar</h1>
 
-      <Card myname="Didar" Narration="My name is Didar"/>
-      <Card myname="Bappy"  />
+      <Card myname="Didar" Narration="My name is Didar" />
+      <Card myname="Bappy" />
     </>
   );
 }
